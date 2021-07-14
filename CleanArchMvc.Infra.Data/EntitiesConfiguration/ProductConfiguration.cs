899fd69fa9f2622918cc4daf9d42e51f85360660
Vinicius Martins <viniciusmv91@gmail.com>
2021-07-14 00:09:49 -0300

    implement DbContext and Configuring Entities

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Price).HasPrecision(10, 2);
+
+            builder.HasOne(p => p.Category).WithMany(c => c.Products).HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
